27817a6ccd98f39edf952c777e1ac0690a30b9ff
Andrew Top <top.od.morning@gmail.com>
2025-03-25 16:13:43 -0400

    Update header.js
    lint, ignore

--- tools/importer/parsers/header.js
+++ tools/importer/parsers/header.js
@@ -9,6 +9,7 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
+// eslint-disable-next-line no-redeclare
 /* global document,WebImporter */
 
 const brandLogoMapping = [
